2da39774f3978aaf50d4205d16f5bf7bc4b15bd5
Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>
2023-07-07 10:54:44 +0200

    Update camService for only in memory
    Signed-off-by: Wiibleyde <98520181+Wiibleyde@users.noreply.github.com>

--- services/camService.py
+++ services/camService.py
@@ -1,76 +1,48 @@
 import json
-import os
 
 class CamService:
-    def __init__(self,filename):
-        self.filename = "data/" + filename
-        if not os.path.exists("data"):
-            os.makedirs("data")
-            self.data = {}
-            self.save()
-        else:
-            self.load()
-
-    def load(self):
-        try:
-            with open(self.filename) as json_file:
-                self.data = json.load(json_file)
-        except:
-            self.data = {}
-            self.save()
-
-    def save(self):
-        with open(self.filename, 'w') as outfile:
-            json.dump(self.data, outfile)
+    def __init__(self):
+        self.data = {}
 
     def setCamVisibleLayer1(self, camId):
         self.set(f"{camId}L1", True)
 
     def hideAllCamLayer1ExceptCam(self, camId):
-        for key in self.data.keys():
-            if key != camId:
-                if key.endswith("L1"):
-                    self.set(key, False)
+        for key in list(self.data.keys()):
+            if key != camId and key.endswith("L1"):
+                del self.data[key]
 
     def setCamVisibleLayer2(self, camId):
         self.set(f"{camId}L2", True)
 
     def hideAllCamLayer2ExceptCam(self, camId):
-        for key in self.data.keys():
-            if key != camId:
-                if key.endswith("L2"):
-                    self.set(key, False)
+        for key in list(self.data.keys()):
+            if key != camId and key.endswith("L2"):
+                del self.data[key]
 
     def setCamVisibleLayer3(self, camId):
         self.set(f"{camId}L3", True)
 
     def hideAllCamLayer3ExceptCam(self, camId):
-        for key in self.data.keys():
-            if key != camId:
-                if key.endswith("L3"):
-                    self.set(key, False)
-        
+        for key in list(self.data.keys()):
+            if key != camId and key.endswith("L3"):
+                del self.data[key]
+
     def delete(self, key):
-        del self.data[key]
-        self.save()
+        if key in self.data:
+            del self.data[key]
 
     def getAll(self):
         return self.data
-    
+
     def clear(self):
         self.data = {}
-        self.save()
 
     def set(self, key, value):
         self.data[key] = value
-        self.save()
 
     def get(self, key):
-        if key in self.data:
-            return self.data[key]
-        else:
-            return False
+        return self.data.get(key, False)
 
     def getKeys(self):
         return self.data.keys()
-    
